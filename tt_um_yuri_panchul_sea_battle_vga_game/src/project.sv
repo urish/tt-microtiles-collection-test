4f741e6e3e72ea3f1457b940b85d2565fefc6f16
Uri Shaked <uri@urishaked.com>
2024-09-24 15:23:17 +0300

    fix two

--- tt_um_yuri_panchul_sea_battle_vga_game/src/project.sv
+++ tt_um_yuri_panchul_sea_battle_vga_game/src/project.sv
@@ -9,10 +9,6 @@ module tt_um_yuri_panchul_sea_battle_vga_game
 (
     input  [7:0] ui_in,    // Dedicated inputs
     output [7:0] uo_out,   // Dedicated outputs
-    input  [7:0] uio_in,   // IOs: Input path
-    output [7:0] uio_out,  // IOs: Output path
-    output [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input        ena,      // always 1 when the design is powered, so you can ignore it
     input        clk,      // clock
     input        rst_n     // reset_n - low to reset
 );
@@ -21,12 +17,9 @@ module tt_um_yuri_panchul_sea_battle_vga_game
 
     // All output pins must be assigned. If not used, assign to 0.
 
-    assign uio_out = '0;
-    assign uio_oe  = '0;
-
     // List all unused inputs to prevent warnings
 
-    wire _unused = & { ena, ui_in [7:2], 1'b0 };
+    wire _unused = & { ui_in [7:2], 1'b0 };
 
     //------------------------------------------------------------------------
 
