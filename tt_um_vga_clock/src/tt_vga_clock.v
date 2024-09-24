549035d8009cf26545db424b4c218c6f473e8129
Uri Shaked <uri@urishaked.com>
2024-09-24 15:27:25 +0300

    fix 4

--- tt_um_vga_clock/src/tt_vga_clock.v
+++ tt_um_vga_clock/src/tt_vga_clock.v
@@ -2,16 +2,9 @@
 module tt_um_vga_clock (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
-
-    assign uio_out = 8'b0;
-    assign uio_oe  = 8'b0;
 
     wire [1:0] R;
     wire [1:0] G;
