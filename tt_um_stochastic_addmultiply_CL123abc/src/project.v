d9dfaa365af25db22d41143b4af7f06a82987257
Uri Shaked <uri@urishaked.com>
2024-09-24 15:38:38 +0300

    fix 8

--- tt_um_stochastic_addmultiply_CL123abc/src/project.v
+++ tt_um_stochastic_addmultiply_CL123abc/src/project.v
@@ -24,10 +24,6 @@
 module tt_um_stochastic_addmultiply_CL123abc(
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
@@ -86,9 +82,6 @@ module tt_um_stochastic_addmultiply_CL123abc(
   // All output pins must be assigned. If not used, assign to 0.
   /*
   assign uo_out[7:0] = mul_avg[8:1];
-  assign uio_out[0] = mul_avg[0];
-  assign uio_out[7:1] = 7'b0;
-  assign uio_oe[7:0] = 8'b1;
   */
   
   assign uo_out[0] = mul_bit_out;
@@ -96,11 +89,9 @@ module tt_um_stochastic_addmultiply_CL123abc(
   assign uo_out[2] = smul_bit_out;
   assign uo_out[3] = clk_counter[17];
   assign uo_out[7:4] = 4'b0;
-  assign uio_out[7:0] = 8'b0;    
-  assign uio_oe[7:0]  = 8'b0;
   
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, ui_in[7:2], uio_in, 1'b0}; 
+  wire _unused = &{ui_in[7:2], 1'b0}; 
 endmodule
 
 /* SUBMODULES:
