5c5d31c2b1e7e6150830203108c54cde0cde47dd
Uri Shaked <uri@urishaked.com>
2024-09-24 15:41:41 +0300

    fix 11

--- tt_um_roy1707018/src/tt_um_roy1707018.v
+++ tt_um_roy1707018/src/tt_um_roy1707018.v
@@ -8,10 +8,6 @@
 module tt_um_roy1707018 (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
@@ -38,11 +34,6 @@ module tt_um_roy1707018 (
 
   // Example: Output assignments (update based on your design needs)
   assign uo_out  = buffer_out ^ {3'b000,ascon_sbox_out};  // Example: take lower 8 bits of buffer_out ^ ascon sbox output
-  assign uio_out = 0;
-  assign uio_oe  = 0;
-
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, uio_in, 1'b0};
 
 endmodule
 
