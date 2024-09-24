872bfe9f5c9bceef808bed21088da3419c38c132
Uri Shaked <uri@urishaked.com>
2024-09-24 15:30:22 +0300

    fix 5 (real)

--- tt_um_tt08_wirecube/src/tt_um_tt08_wirecube.sv
+++ tt_um_tt08_wirecube/src/tt_um_tt08_wirecube.sv
@@ -7,10 +7,6 @@ module tt_um_tt08_wirecube (
     // Tiny Tapeout digital interface
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // will go high when the design is enabled
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
@@ -63,18 +59,4 @@ module tt_um_tt08_wirecube (
     assign uo_out[6] = B[0];
     assign uo_out[7] = hsync;
     
-    // Bidir PMOD - SPI and additional signals
-    
-    // Top row
-    /*assign TODO   = uio_in[0];*/  assign uio_oe[0] = 1'b0; assign uio_out[0] = 1'b0;
-    /*assign TODO   = uio_in[1];*/  assign uio_oe[1] = 1'b0; assign uio_out[1] = 1'b0;
-    /*assign TODO   = uio_in[2];*/  assign uio_oe[2] = 1'b1; assign uio_out[2] = 1'b0;
-    /*assign TODO   = uio_in[3];*/  assign uio_oe[3] = 1'b0; assign uio_out[3] = 1'b0;
-
-    // Bottom row
-    assign uio_out[4] = 1'b0; assign uio_oe[4] = 1'b0;
-    assign uio_out[5] = 1'b0; assign uio_oe[5] = 1'b0;
-    assign uio_out[6] = 1'b0; assign uio_oe[6] = 1'b0;
-    assign uio_out[7] = 1'b0; assign uio_oe[7] = 1'b0;
-
 endmodule
