593a202116fb1fe8607241943f0215df352365f7
Uri Shaked <uri@urishaked.com>
2024-09-24 15:36:04 +0300

    fix 7

--- tt_um_thexeno_rgbw_controller/src/rgbw_lamp.v
+++ tt_um_thexeno_rgbw_controller/src/rgbw_lamp.v
@@ -15,10 +15,6 @@
 module tt_um_thexeno_rgbw_controller (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // will go high when the design is enabled
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
@@ -67,12 +63,10 @@ module tt_um_thexeno_rgbw_controller (
 
 
     // List all unused inputs to prevent warnings
-    wire _unused = &{ena, ui_in[7], ui_in[2:0], uio_in[7:0], 1'b0};
+    wire _unused = &{ui_in[7], ui_in[2:0], 1'b0};
 
     // assign all the IO to the relative wires/regs
     // uo, uio
-    assign uio_out = uo_out_reg;
-    assign uio_oe = 8'hff;
     assign uo_out[7:4] = 0;
     assign uo_out[0] = red_pin;
     assign uo_out[1] = green_pin;
