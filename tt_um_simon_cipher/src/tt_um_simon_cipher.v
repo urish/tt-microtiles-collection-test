5eccac2f32e38df4273cd518edb240df4037f9df
Uri Shaked <uri@urishaked.com>
2024-09-24 15:39:09 +0300

    fix 9

--- tt_um_simon_cipher/src/tt_um_simon_cipher.v
+++ tt_um_simon_cipher/src/tt_um_simon_cipher.v
@@ -8,10 +8,6 @@
 module tt_um_simon_cipher (
 						  input wire [7:0] 	ui_in,   // Dedicated inputs
 						  output wire [7:0] uo_out,  // Dedicated outputs
-						  input wire [7:0] 	uio_in,  // IOs: Input path
-						  output wire [7:0] uio_out, // IOs: Output path
-						  output wire [7:0] uio_oe,  // IOs: Enable path (active high: 0=input, 1=output)
-						  input wire 		ena,     // always 1 when the design is powered, so you can ignore it
 						  input wire 		clk,     // clock
 						  input wire 		rst_n    // reset_n - low to reset
 						  );
@@ -40,10 +36,8 @@ module tt_um_simon_cipher (
 								 );
    
    assign uo_out[6:1] = 6'b0;
-   assign uio_out = 7'b0;
-   assign uio_oe  = 7'b0;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, ui_in[5:1], 1'b0};
+  wire _unused = &{ui_in[5:1], 1'b0};
 
 endmodule
