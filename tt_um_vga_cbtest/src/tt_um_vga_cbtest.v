9f59b67eadbe62a9a35a04b6109f50b3189d922c
Uri Shaked <uri@urishaked.com>
2024-09-24 15:28:05 +0300

    fix 5

--- tt_um_vga_cbtest/src/tt_um_vga_cbtest.v
+++ tt_um_vga_cbtest/src/tt_um_vga_cbtest.v
@@ -8,10 +8,6 @@
 module tt_um_vga_cbtest  (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
@@ -29,12 +25,8 @@ module tt_um_vga_cbtest  (
   // TinyVGA PMOD
   assign uo_out  = {hsync, B[0], G[0], R[0], vsync, B[1], G[1], R[1]};
 
-  // Unused outputs assigned to 0.
-  assign uio_out = 0;
-  assign uio_oe  = 0;
-
   // Suppress unused signals warning
-  wire _unused_ok = &{ena, ui_in, uio_in};
+  wire _unused_ok = &{ui_in};
 
   vga_sync_generator vga_sync_gen (
       .clk(clk),
